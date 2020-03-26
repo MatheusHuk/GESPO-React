fa1a0dbe81bf5c6de4f3c22996d647ea9a10a322
MatheusHuk <matheushukmoreschi2000@gmail.com>
2020-03-26 15:53:42 -0300

    Mais rotas

--- src/App.js
+++ src/App.js
@@ -15,8 +15,8 @@ function App() {
 
   return (
     <>
-      <Comp.Menu />
-      <Comp.SideMenu route={route}/>
+      <Comp.Menu goto={goTo} />
+      <Comp.SideMenu route={route} goto={goTo}/>
       <div class="todo">
         <Routes goto={goTo}/>
       </div>
--- src/components/sideMenu/index.jsx
+++ src/components/sideMenu/index.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 import * as Style from './style'
 
-export default function SideMenu({route}){
+export default function SideMenu({ route, goto }){
 
     return(
         <Style.SideMenu>
