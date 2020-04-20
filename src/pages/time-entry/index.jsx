1f2078554d8fbdae1a5db6a689daf4c3f1fe8eba
MatheusHuk <matheushukmoreschi2000@gmail.com>
2020-04-20 20:34:55 -0300

    Nova caixa de filtros

--- src/pages/time-entry/index.jsx
+++ src/pages/time-entry/index.jsx
@@ -8,6 +8,9 @@ export default function TimeEntry({ setLoad }) {
 
     const [sel, setSel] = useState(false);
 
+    const [showFiltros, setShowFiltros] = useState(true);
+    const [showApontamento, setShowApontamento] = useState(false);
+
     useEffect(() => {
         setLoad(true);
         setLoad(false);
@@ -16,74 +19,85 @@ export default function TimeEntry({ setLoad }) {
     return (
         <>
             <Viewer>
-                <Style.Container>
-                    <Style.Filtros>
-                        <Style.Header>
-                            Filtros
-                        </Style.Header>
-                        <Style.Body>
-                            <Style.BoxBody>
-                                <Style.Box>
-                                    <Style.BoxHeader>
-                                        Time
-                                </Style.BoxHeader>
-                                    <Dropdown>
-                                        <Dropdown.Toggle className="drop-down">
-                                            {
-                                                sel ? sel : "Select"
-                                            }
-                                        </Dropdown.Toggle>
+                {showFiltros ?
+                    <Style.Container selected={showFiltros}>
+                        <Style.Filtros>
+                            <Style.FHeader>Filtros</Style.FHeader>
+                            <Style.FBody>
+                                <Style.BoxBody>
+                                    <Style.Box>
+                                        <Style.BoxHeader>
+                                            Time
+                                        </Style.BoxHeader>
+                                        <Dropdown>
+                                            <Dropdown.Toggle className="drop-down">
+                                                {
+                                                    sel ? sel : "Select"
+                                                }
+                                            </Dropdown.Toggle>
 
-                                        <Dropdown.Menu>
-                                            <Dropdown.Item onClick={() => { setSel("1") }}>Action</Dropdown.Item>
-                                            <Dropdown.Item onClick={() => { setSel("2") }}>Another action</Dropdown.Item>
-                                            <Dropdown.Item onClick={() => { setSel("3") }}>Something else</Dropdown.Item>
-                                        </Dropdown.Menu>
-                                    </Dropdown>
-                                </Style.Box>
-                                <Style.Box>
-                                    <Style.BoxHeader>
-                                        Centro de Custo
-                                </Style.BoxHeader>
-                                    <Dropdown>
-                                        <Dropdown.Toggle className="drop-down">
-                                            {
-                                                sel ? sel : "Select"
-                                            }
-                                        </Dropdown.Toggle>
+                                            <Dropdown.Menu>
+                                                <Dropdown.Item onClick={() => { setSel("1") }}>Action</Dropdown.Item>
+                                                <Dropdown.Item onClick={() => { setSel("2") }}>Another action</Dropdown.Item>
+                                                <Dropdown.Item onClick={() => { setSel("3") }}>Something else</Dropdown.Item>
+                                            </Dropdown.Menu>
+                                        </Dropdown>
+                                    </Style.Box>
+                                    <Style.Box>
+                                        <Style.BoxHeader>
+                                            Centro de Custo
+                                        </Style.BoxHeader>
+                                        <Dropdown>
+                                            <Dropdown.Toggle className="drop-down">
+                                                {
+                                                    sel ? sel : "Select"
+                                                }
+                                            </Dropdown.Toggle>
 
-                                        <Dropdown.Menu>
-                                            <Dropdown.Item onClick={() => { setSel("1") }}>Action</Dropdown.Item>
-                                            <Dropdown.Item onClick={() => { setSel("2") }}>Another action</Dropdown.Item>
-                                            <Dropdown.Item onClick={() => { setSel("3") }}>Something else</Dropdown.Item>
-                                        </Dropdown.Menu>
-                                    </Dropdown>
-                                </Style.Box>
-                                <Style.Box>
-                                    <Style.BoxHeader>
-                                        Projeto
-                                </Style.BoxHeader>
-                                    <Dropdown>
-                                        <Dropdown.Toggle className="drop-down">
-                                            {
-                                                sel ? sel : "Select"
-                                            }
-                                        </Dropdown.Toggle>
+                                            <Dropdown.Menu>
+                                                <Dropdown.Item onClick={() => { setSel("1") }}>Action</Dropdown.Item>
+                                                <Dropdown.Item onClick={() => { setSel("2") }}>Another action</Dropdown.Item>
+                                                <Dropdown.Item onClick={() => { setSel("3") }}>Something else</Dropdown.Item>
+                                            </Dropdown.Menu>
+                                        </Dropdown>
+                                    </Style.Box>
+                                    <Style.Box>
+                                        <Style.BoxHeader>
+                                            Projeto
+                                        </Style.BoxHeader>
+                                        <Dropdown>
+                                            <Dropdown.Toggle className="drop-down">
+                                                {
+                                                    sel ? sel : "Select"
+                                                }
+                                            </Dropdown.Toggle>
 
-                                        <Dropdown.Menu>
-                                            <Dropdown.Item onClick={() => { setSel("1") }}>Action</Dropdown.Item>
-                                            <Dropdown.Item onClick={() => { setSel("2") }}>Another action</Dropdown.Item>
-                                            <Dropdown.Item onClick={() => { setSel("3") }}>Something else</Dropdown.Item>
-                                        </Dropdown.Menu>
-                                    </Dropdown>
-                                </Style.Box>
-                            </Style.BoxBody>
-                            <Style.Footer>
-                                    <Button className="but">Filtrar</Button>
-                            </Style.Footer>
-                        </Style.Body>
-                    </Style.Filtros>
-                </Style.Container>
+                                            <Dropdown.Menu>
+                                                <Dropdown.Item onClick={() => { setSel("1") }}>Action</Dropdown.Item>
+                                                <Dropdown.Item onClick={() => { setSel("2") }}>Another action</Dropdown.Item>
+                                                <Dropdown.Item onClick={() => { setSel("3") }}>Something else</Dropdown.Item>
+                                            </Dropdown.Menu>
+                                        </Dropdown>
+                                    </Style.Box>
+                                </Style.BoxBody>
+                            </Style.FBody>
+                            <Style.FFooter>
+                                <Button className="but">Filtrar</Button>
+                            </Style.FFooter>
+                        </Style.Filtros>
+                        <Style.Dados>
+                            <Style.DHeader>Novo Apontamento</Style.DHeader>
+                            <Style.DBody></Style.DBody>
+                            <Style.DFooter>
+                                <Button className="but">Gravar</Button>
+                            </Style.DFooter>
+                        </Style.Dados>
+                    </Style.Container>
+                    :
+                    <Style.Container>
+                        <Style.Filtros />
+                    </Style.Container>
+                }
             </Viewer>
         </>
     );
--- src/pages/time-entry/style.js
+++ src/pages/time-entry/style.js
@@ -10,49 +10,56 @@ const entranceAnimation = keyframes`
 `;
 export const Container = styled.div`
     position: relative;
-    width: 100%;
+    width: 100VW;
     height: 100%;
     left: 0%;
     animation: ${entranceAnimation} 0.5s;
 `;
 
 export const Filtros = styled.div`
+    padding: 2%;
     left: 50%;
-    top: 5%;
+    top: 3%;
     transform: translateX(-50%);
-    position: absolute;
+    position: relative;
     height: 30%;
     width: 90%;
     border-radius: 17px;
     background-color: #2B47AA;
+    margin-bottom: 4%;
 `;
 
-export const Header = styled.div`
+export const FHeader = styled.div`
     padding: 0 2% 0 2%;
-    line-height: 2;
-    position: absolute;
+    line-height: 50%;
+    position: relative;
     left: 50%;
     transform: translateX(-50%);
-    width: 95%;
-    height: 20%;
+    width: 100%;
+    height: 3VH;
     border-bottom: 1px solid white;
     color: white;
-    font-size: 3VH;
+    font-size: 2.5VH;
 `;
 
-export const Body = styled.div`
-    position: absolute;
-    left: 50%;
-    top: 20%;
-    transform: translate(-50%, 0);
-    width: 95%;
-    height: 70%;
+export const FBody = styled.div`
+    position: relative;
+    width: 100%;
+    height: calc(100% - 6VH);
     display: flex;
     justify-content: space-around;
 `;
 
+export const FFooter = styled.div`
+    position: relative;
+    width: 100%;
+    height: 3VH;
+    display: flex;
+    justify-content: right;
+`;
+
 export const BoxBody = styled.div`
-    position: absolute;
+    position: relative;
     left: 50%;
     transform: translate(-50%, 0);
     width: 100%;
@@ -61,17 +68,8 @@ export const BoxBody = styled.div`
     justify-content: space-around;
 `;
 
-export const Footer = styled.div`
-    position: absolute;
-    top: 80%;
-    width: 100%;
-    height: 20%;
-    display: flex;
-    justify-content: right;
-`;
-
 export const BoxHeader = styled.div`
-    font-size: 1.5VW;
+    font-size: 80%;
     overflow: Hidden;
     width: 100%;
     height: 50%;
@@ -85,4 +83,47 @@ export const Box = styled.div`
     top: 10%;
     width: 25%;
     height: 60%;
+`;
+
+export const Dados = styled.div`
+    padding: 2%;
+    left: 50%;
+    transform: translateX(-50%);
+    position: relative;
+    height: 60%;
+    width: 90%;
+    border-radius: 17px;
+    background-color: #2B47AA;
+`;
+
+export const DHeader = styled.div`
+    padding: 0 2% 0 2%;
+    line-height: 50%;
+    position: relative;
+    left: 50%;
+    transform: translateX(-50%);
+    width: 100%;
+    height: 3VH;
+    border-bottom: 1px solid white;
+    color: white;
+    font-size: 2.5VH;
+`;
+
+export const DBody = styled.div`
+    position: relative;
+    left: 50%;
+    transform: translate(-50%, 0);
+    width: 100%;
+    height: calc(100% - 6VH);
+    display: flex;
+    background-color: green;
+    justify-content: space-around;
+`;
+
+export const DFooter = styled.div`
+    position: relative;
+    width: 100%;
+    height: 3VH;
+    display: flex;
+    justify-content: right;
 `;
