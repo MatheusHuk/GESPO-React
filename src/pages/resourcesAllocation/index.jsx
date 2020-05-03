1c6acf07334ae76d82391664db4cad7e97359add
bruno1903 <brunoalmeida1960@gmail.com>
2020-05-03 17:35:37 -0300

    Adiciona formularios

---
+++ src/pages/resourcesAllocation/index.jsx
@@ -0,0 +1,87 @@
+import React, { useEffect } from 'react'
+import Viewer from '../../Layout/Viewer'
+import {FormControl, FormGroup, FormLabel, Form, Col, Button, Card} from 'react-bootstrap';
+import "./index.css"
+
+export default function ResourcesAllocation(){
+    
+    return(
+        <>
+        
+            <Viewer>
+            <div class="title">GESPO</div>
+                <div class="subtitle">Cadastros/ Cadastro de Projeto </div>
+                <div className = "divFormulario">
+                    <Card >
+                        <div className = "divHeader">Selecione o Projeto</div>
+                        <Card.Body className = "fundoForm">
+                        <Form>
+                            <Form.Row>
+                                <Form.Group as={Col} controlId="formGridProjeto">
+                                <Form.Label>Projeto</Form.Label>
+                                <Form.Control as="select" value="Choose...">
+                                <option>Selecione...</option>
+                                <option>Gerente 1</option>
+                                <option>Gerente 2</option>
+                                <option>Gerente 3</option>
+                                </Form.Control>
+                                </Form.Group>
+                            </Form.Row>
+
+                        
+                        </Form>     
+                        </Card.Body>
+                    </Card>
+                </div>
+                    <div>
+                        <div className = "divFormulario2">
+                            <Card>
+                                <div className = "divHeader">Dados Alocação</div>
+                                <Card.Body className = "fundoForm">
+                        <Form>   
+
+                            <Form.Row>
+
+                                <Form.Group as={Col} controlId="formGridCliente">
+                                <Form.Label>Categoria</Form.Label>
+                                <Form.Control as="select" value="Choose...">
+                                <option>Selecione...</option>
+                                <option>Cliente 1</option>
+                                <option>Cliente 2</option>
+                                <option>Cliente 3</option>
+                                </Form.Control>
+                                </Form.Group>
+
+                                <Form.Group as={Col} controlId="formGridCCusto">
+                                <Form.Label>Recurso</Form.Label>
+                                <Form.Control as="select" value="Choose..." >
+                                <option>Selecione...</option>
+                                <option>Categoria 1</option>
+                                <option>Categoria 2</option>
+                                <option>Categoria 3</option>
+                                </Form.Control>
+                                </Form.Group>
+
+                                <Form.Group as={Col} controlId="formGridCategoria">
+                                <Form.Label>Time</Form.Label>
+                                <Form.Control as="select" value="Choose...">
+                                <option>Selecione...</option>
+                                <option>Categoria 1</option>
+                                <option>Categoria 2</option>
+                                <option>Categoria 3</option>
+                                </Form.Control>
+                                </Form.Group>
+
+                            </Form.Row>
+
+                            
+                        </Form>     
+                        </Card.Body>
+                            </Card>
+                        </div>
+                    </div>
+            </Viewer>
+
+        </>
+    );
+}
--- src/pages/three/index.jsx
+++ src/pages/three/index.jsx
@@ -16,7 +16,7 @@ export default function Three(){
                         <div class="component" onClick={() => history.push("/two") }>Rota 2</div>
                     </div>
                     <div class="subcontainer">
-                        <div class="component" onClick={() => history.push("/three") }>Rota 3</div>
+                        <div class="component" onClick={() => history.push("/three/resourcesAllocation") }>Alocação de Recursos</div>
                         <div class="component" onClick={() => history.push("/four") }>Rota 4</div>
                     </div>
                 </div>
