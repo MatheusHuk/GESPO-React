54011a70daf27d936eeddeeeed1e5b8b350d4e83
MatheusHuk <matheushukmoreschi2000@gmail.com>
2020-06-19 22:52:19 -0300

    Env production and style

---
+++ src/pages/style.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components'
+
+export const Invalid = styled.div`
+    width: 100%;
+    height: auto;
+    background-color: #3498db;
+    padding: 4%;
+    color: white;
+    border-radius: 10px;
+    text-align: center;
+`
